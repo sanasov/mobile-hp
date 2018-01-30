400c89e85bed8be4d3e32e630db567602fb1f1da
sanasov <igrey@mail.ru>
2018-01-31 00:41:03 +0300

    add holiday engine

--- mobile-app/src/app/service/HolidayEngine.ts
+++ mobile-app/src/app/service/HolidayEngine.ts
@@ -1,12 +1,139 @@
+import {CalendarEvent} from "angular-calendar";
+
 export class HolidayEngine {
 
-  holidayDate(eventDate: Date, daysAmount: number) {
-    let result = new Date();
-    result.setDate(eventDate.getDate() + daysAmount);
-    return result;
-  }
+    pi: string = "3.14159265359";
+    exp: string = "2.71828182846";
+    colors: any = {
+        red: {
+            primary: '#ad2121',
+            secondary: '#FAE3E3'
+        },
+        blue: {
+            primary: '#1e90ff',
+            secondary: '#D1E8FF'
+        },
+        yellow: {
+            primary: '#e3bc08',
+            secondary: '#FDF1BA'
+        }
+    };
 
-  getRoundedNumbers(eventDate: Date) {
+    hpEvents: Array<{ title: string, date: string }>;
 
-  }
+    constructor(hpEvents: Array<{ title: string, date: string }>) {
+        this.hpEvents = hpEvents;
+    }
+
+    public getCalendarEvents(year: number): Array<CalendarEvent> {
+        let calendarEvents: CalendarEvent[] = [];
+        this.hpEvents.forEach(hpEvent => {
+            calendarEvents = calendarEvents.concat(this.getCalendarEventsByMagicNumbers(hpEvent, year));
+        });
+        return calendarEvents
+    }
+
+    private getCalendarEventsByMagicNumbers(hpEvent, year) {
+        let eventDate = new Date(hpEvent.date);
+        return this.getMagicNumbersCorrespondYear(eventDate, year).map(magicNumber => {
+            return {
+                start: this.holidayDate(eventDate, magicNumber.daysAmount),
+                end: this.holidayDate(eventDate, magicNumber.daysAmount),
+                title: magicNumber.title + "\n" + hpEvent.title + " days: " + magicNumber.daysAmount,
+                color: this.colors.red,
+                actions: null
+            }
+        });
+    }
+
+    private getMagicNumbersCorrespondYear(eventDate: Date, year: number): Array<{ daysAmount: number, title: string }> {
+        const firstDayOfYear = new Date(year, 0, 1);
+        const lastDayOfYear = new Date(year, 11, 31);
+        const diffDaysStartYear = this.diffDays(eventDate, firstDayOfYear);
+        const diffDaysEndYear = this.diffDays(eventDate, lastDayOfYear);
+        let magicNumbers: Array<{ daysAmount: number, title: string }> = [];
+        for (let i = diffDaysStartYear; i <= diffDaysEndYear; i++) {
+            if (this.isRounded(i)) {
+                magicNumbers.push({daysAmount: i, title: "Круглая дата"});
+            } else if (this.allNumbersAreTheSame(i)) {
+                magicNumbers.push({daysAmount: i, title: "Из одной цифры"});
+            } else if (i >= Math.pow(2, 10) && this.isTwoInDegree(i)) {
+                magicNumbers.push({daysAmount: i, title: "2 в степени"});
+            } else if (i >= Math.pow(3, 7) && this.isThreeInDegree(i)) {
+                magicNumbers.push({daysAmount: i, title: "3 в степени"});
+            } else if (this.isFactorial(i)) {
+                magicNumbers.push({daysAmount: i, title: "Факториал"});
+            } else if (this.isPi(i)) {
+                magicNumbers.push({daysAmount: i, title: "Pi! pi = ${this.pi}"});
+            } else if (this.isExp(i)) {
+                magicNumbers.push({daysAmount: i, title: "Экспонента! e = ${this.exp}"});
+            }
+        }
+        return magicNumbers;
+    }
+
+    private holidayDate(eventDate: Date, daysAmount: number) {
+        let result = new Date(eventDate);
+        result.setDate(eventDate.getDate() + daysAmount);
+        return result;
+    }
+
+    private diffDays(date1: Date, date2: Date) {
+        var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+        return Math.ceil(timeDiff / (1000 * 3600 * 24));
+    }
+
+    private isRounded(daysAmount: number) {
+        if (daysAmount <= 1000) {
+            return daysAmount % 100 === 0;
+        } else if (daysAmount <= 1500) {
+            return daysAmount % 500 === 0;
+        }
+        return daysAmount % 1000 === 0;
+    }
+
+    private allNumbersAreTheSame(daysAmount: number) {
+        const daysAmountString = daysAmount.toString();
+        for (let i = 1; i < daysAmountString.length; i++) {
+            if (daysAmountString[i] != daysAmountString[0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private isPi(daysAmount: number) {
+        const daysAmountString = daysAmount.toString();
+        return this.pi.replace(".", "").substr(0, daysAmountString.length) === daysAmountString;
+    }
+
+    private isExp(daysAmount: number) {
+        const daysAmountString = daysAmount.toString();
+        return this.exp.replace(".", "").substr(0, daysAmountString.length) === daysAmountString;
+    }
+
+    private isTwoInDegree(i: number) {
+        while (i > 1) {
+            if (i % 2 !== 0) {
+                return false;
+            }
+            i = i / 2;
+        }
+        return i === 1;
+    }
+
+    private isThreeInDegree(i: number) {
+        while (i > 1) {
+            if (i % 3 !== 0) {
+                return false;
+            }
+            i = i / 3;
+        }
+        return i === 1;
+    }
+
+    private isFactorial(i: number): boolean {
+        const sevenFact = 5040;
+        return i === sevenFact || i === sevenFact * 8 || i === sevenFact * 8 * 9 || i === sevenFact * 8 * 9 * 10;
+    }
 }
--- mobile-app/src/pages/calendar/calendar.ts
+++ mobile-app/src/pages/calendar/calendar.ts
@@ -1,154 +1,154 @@
 import {Component, TemplateRef, ViewChild} from '@angular/core';
+import {Storage} from '@ionic/storage';
 import {
-  CalendarEvent,
-  CalendarEventAction,
-  CalendarEventTimesChangedEvent
+    CalendarEvent,
+    CalendarEventAction,
+    CalendarEventTimesChangedEvent
 } from 'angular-calendar';
 import {Subject} from "rxjs/Subject";
+import {HolidayEngine} from "../../app/service/HolidayEngine";
 
 @Component({
-  selector: 'page-calendar',
-  templateUrl: 'calendar.html'
+    selector: 'page-calendar',
+    templateUrl: 'calendar.html'
 })
 
 export class CalendarPage {
-  @ViewChild('modalContent') modalContent: TemplateRef<any>;
-  @ViewChild('customHeaderTemplate') customHeaderTemplate: TemplateRef<any>;
+    @ViewChild('modalContent') modalContent: TemplateRef<any>;
+    @ViewChild('customHeaderTemplate') customHeaderTemplate: TemplateRef<any>;
 
-  view: string = 'month';
+    view: string = 'month';
+    holidayEngine: HolidayEngine;
+    viewDate: Date = new Date();
+    currentYear: Number = new Date().getFullYear();
+    weekendDays: Array<String> = ["a", "b", "c", "d", "c", "d", "c"];
 
-  viewDate: Date = new Date();
-  weekendDays: Array<String> = ["a", "b", "c", "d", "c", "d", "c"];
-  colors: any = {
-    red: {
-      primary: '#ad2121',
-      secondary: '#FAE3E3'
-    },
-    blue: {
-      primary: '#1e90ff',
-      secondary: '#D1E8FF'
-    },
-    yellow: {
-      primary: '#e3bc08',
-      secondary: '#FDF1BA'
+
+    constructor(private storage: Storage) {
+        storage.get('events').then((result) => {
+            this.holidayEngine = new HolidayEngine(result || []);
+            this.events = this.holidayEngine.getCalendarEvents(2019)
+                .concat(this.holidayEngine.getCalendarEvents(2017))
+                .concat(this.holidayEngine.getCalendarEvents(2016))
+                .concat(this.holidayEngine.getCalendarEvents(2015))
+                .concat(this.holidayEngine.getCalendarEvents(2014));
+        });
     }
-  };
 
-  modalData: {
-    action: string;
-    event: CalendarEvent;
-  };
 
-  actions: CalendarEventAction[] = [
-    {
-      label: '<i class="fa fa-fw fa-pencil"></i>',
-      onClick: ({event}: { event: CalendarEvent }): void => {
-        this.handleEvent('Edited', event);
-      }
-    },
-    {
-      label: '<i class="fa fa-fw fa-times"></i>',
-      onClick: ({event}: { event: CalendarEvent }): void => {
-        this.events = this.events.filter(iEvent => iEvent !== event);
-        this.handleEvent('Deleted', event);
-      }
+    modalData: {
+        action: string;
+        event: CalendarEvent;
+    };
+
+    actions: CalendarEventAction[] = [
+        {
+            label: '<i class="fa fa-fw fa-pencil"></i>',
+            onClick: ({event}: { event: CalendarEvent }): void => {
+                this.handleEvent('Edited', event);
+            }
+        },
+        {
+            label: '<i class="fa fa-fw fa-times"></i>',
+            onClick: ({event}: { event: CalendarEvent }): void => {
+                this.events = this.events.filter(iEvent => iEvent !== event);
+                this.handleEvent('Deleted', event);
+            }
+        }
+    ];
+
+    refresh: Subject<any> = new Subject();
+
+    events: CalendarEvent[] = [
+        // {
+        //     start: new Date(),
+        //     end: new Date(),
+        //     title: 'A 3 day event',
+        //     color: this.colors.red,
+        //     actions: this.actions
+        // }
+        // {
+        //     start: startOfDay(new Date()),
+        //     title: 'An event with no end date',
+        //     color: colors.yellow,
+        //     actions: this.actions
+        // },
+        // {
+        //     start: subDays(endOfMonth(new Date()), 3),
+        //     end: addDays(endOfMonth(new Date()), 3),
+        //     title: 'A long event that spans 2 months',
+        //     color: colors.blue
+        // },
+        // {
+        //     start: addHours(startOfDay(new Date()), 2),
+        //     end: new Date(),
+        //     title: 'A draggable and resizable event',
+        //     color: colors.yellow,
+        //     actions: this.actions,
+        //     resizable: {
+        //         beforeStart: true,
+        //         afterEnd: true
+        //     },
+        //     draggable: true
+        // }
+    ];
+
+    activeDayIsOpen: boolean = false;
+
+    dayClicked({date, events}: { date: Date; events: CalendarEvent[] }): void {
+        // if (isSameMonth(date, this.viewDate)) {
+        //     if (
+        //         (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        //         events.length === 0
+        //     ) {
+        //         this.activeDayIsOpen = false;
+        //     } else {
+        //         this.activeDayIsOpen = true;
+        //         this.viewDate = date;
+        //     }
+        // }
     }
-  ];
 
-  refresh: Subject<any> = new Subject();
-
-  events: CalendarEvent[] = [
-    {
-      start: new Date(),
-      end: new Date(),
-      title: 'A 3 day event',
-      color: this.colors.red,
-      actions: this.actions
+    eventTimesChanged({
+                          event,
+                          newStart,
+                          newEnd
+                      }: CalendarEventTimesChangedEvent): void {
+        event.start = newStart;
+        event.end = newEnd;
+        this.handleEvent('Dropped or resized', event);
+        this.refresh.next();
     }
-    // {
-    //     start: startOfDay(new Date()),
-    //     title: 'An event with no end date',
-    //     color: colors.yellow,
-    //     actions: this.actions
-    // },
-    // {
-    //     start: subDays(endOfMonth(new Date()), 3),
-    //     end: addDays(endOfMonth(new Date()), 3),
-    //     title: 'A long event that spans 2 months',
-    //     color: colors.blue
-    // },
-    // {
-    //     start: addHours(startOfDay(new Date()), 2),
-    //     end: new Date(),
-    //     title: 'A draggable and resizable event',
-    //     color: colors.yellow,
-    //     actions: this.actions,
-    //     resizable: {
-    //         beforeStart: true,
-    //         afterEnd: true
-    //     },
-    //     draggable: true
-    // }
-  ];
 
-  activeDayIsOpen: boolean = false;
-
-  // constructor(private modal: NgbModal) {}
-
-  dayClicked({date, events}: { date: Date; events: CalendarEvent[] }): void {
-    // if (isSameMonth(date, this.viewDate)) {
-    //     if (
-    //         (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
-    //         events.length === 0
-    //     ) {
-    //         this.activeDayIsOpen = false;
-    //     } else {
-    //         this.activeDayIsOpen = true;
-    //         this.viewDate = date;
-    //     }
-    // }
-  }
-
-  eventTimesChanged({
-                      event,
-                      newStart,
-                      newEnd
-                    }: CalendarEventTimesChangedEvent): void {
-    event.start = newStart;
-    event.end = newEnd;
-    this.handleEvent('Dropped or resized', event);
-    this.refresh.next();
-  }
-
-  handleEvent(action: string, event: CalendarEvent): void {
-    this.modalData = {event, action};
-    // this.modal.open(this.modalContent, { size: 'lg' });
-  }
-
-  addEvent(): void {
-    // this.events.push({
-    //     title: 'New event',
-    //     start: startOfDay(new Date()),
-    //     end: endOfDay(new Date()),
-    //     color: colors.red,
-    //     draggable: true,
-    //     resizable: {
-    //         beforeStart: true,
-    //         afterEnd: true
-    //     }
-    // });
-    this.refresh.next();
-  }
-
-
-  changeMonth($event): void {
-    if (Math.abs($event.deltaX) < 100) {
-      return;
+    handleEvent(action: string, event: CalendarEvent): void {
+        this.modalData = {event, action};
+        // this.modal.open(this.modalContent, { size: 'lg' });
     }
-    if ($event.deltaX < 0) {
-      document.getElementById("calendarNextMonth").click();
-    } else {
-      document.getElementById("calendarPreviousMonth").click();
+
+    addEvent(): void {
+        // this.events.push({
+        //     title: 'New event',
+        //     start: startOfDay(new Date()),
+        //     end: endOfDay(new Date()),
+        //     color: colors.red,
+        //     draggable: true,
+        //     resizable: {
+        //         beforeStart: true,
+        //         afterEnd: true
+        //     }
+        // });
+        this.refresh.next();
     }
-  }
+
+
+    changeMonth($event): void {
+        if (Math.abs($event.deltaX) < 100) {
+            return;
+        }
+        if ($event.deltaX < 0) {
+            document.getElementById("calendarNextMonth").click();
+        } else {
+            document.getElementById("calendarPreviousMonth").click();
+        }
+    }
 }

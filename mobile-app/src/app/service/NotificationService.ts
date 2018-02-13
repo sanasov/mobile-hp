2a1f0e8888de821d7975081c3817a86e91c55286
sanasov <sanasov@phoenixit.ru>
2018-02-13 10:32:12 +0300

    trigger click on notification

--- mobile-app/src/app/service/NotificationService.ts
+++ mobile-app/src/app/service/NotificationService.ts
@@ -30,6 +30,9 @@ export class NotificationService {
     }
 
     public initCordovaNotifications(holidays): void {
+      if (!window['cordova']) {
+        return;
+      }
         cordova.plugins.notification.local.schedule({
             title: 'My first notification',
             text: 'Thats pretty easy...',

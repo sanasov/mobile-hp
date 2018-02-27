9f8b041a313ad2504c2d56414fd6f53c13d39fb0
sanasov <sanasov@phoenixit.ru>
2018-02-27 12:23:23 +0300

    z-index and relative position

--- mobile-app/src/app/component/animations/animations.component.ts
+++ mobile-app/src/app/component/animations/animations.component.ts
@@ -1,22 +1,28 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {trigger, style, transition, animate, keyframes, query, stagger} from '@angular/animations';
+import {trigger, style, transition, animate, keyframes, query, stagger, state} from '@angular/animations';
 
 @Component({
   selector: 'app-animations',
   templateUrl: './animations.component.html',
   animations: [
 
-    trigger('goals', [
-      transition('* => *', [
+    trigger('turnOn', [
+      state('true', style({  'background-color': 'white' })),
+      transition('false => true', [
 
-        query('h1', style({opacity: 0, color:"red"}), {optional: true}),
+        query('h1', style({opacity: 0, color: "red"}), {optional: true}),
 
         query('h1', stagger('300ms', [
-          animate('.6s ease-in', keyframes([
+          animate('3s ease-in', keyframes([
             style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
             style({opacity: .5, transform: 'translateY(35px)', offset: 0.3}),
-            style({opacity: 1, transform: 'translateY(0)', offset: 1.0}),
-          ]))]), {optional: true})
+            style({opacity: 1, transform: 'translateY(0)', offset: 0.31}),
+            style({height: '100px', offset: 0.5}),
+            style({height: '30px', offset: 0.7}),
+            style({color: 'red', offset: 1.0}),
+          ]))
+        ]), {optional: true}),
+
       ])
     ])
 
@@ -24,9 +30,13 @@ import {trigger, style, transition, animate, keyframes, query, stagger} from '@a
 })
 export class AnimationsComponent implements OnInit {
 
-  goals = ['My first life goal', 'I want to climb a mountain', 'Go ice skiing'];
+  turnOn: boolean = false;
 
   constructor() {
+  }
+
+  animate() {
+    this.turnOn = !this.turnOn;
   }
 
   ngOnInit() {

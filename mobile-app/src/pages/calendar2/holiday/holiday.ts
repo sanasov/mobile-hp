5b907fc945c9209a59a4b4d0c57a211ff3b25ffc
sanasov <sanasov@phoenixit.ru>
2018-03-02 13:07:51 +0300

    days past

--- mobile-app/src/pages/calendar2/holiday/holiday.ts
+++ mobile-app/src/pages/calendar2/holiday/holiday.ts
@@ -1,7 +1,6 @@
 import {NavParams, Platform} from "ionic-angular";
 import {Component} from "@angular/core";
 import {TranslateService} from "@ngx-translate/core";
-import {CalendarHolidaySlider} from "../../../app/service/CalendarHolidaySlider";
 import {WorldHoliday} from "../../../app/dictionary/WorldHoliday";
 import HappyHoliday from "../../../app/domain/happy-holiday";
 
@@ -10,34 +9,23 @@ import HappyHoliday from "../../../app/domain/happy-holiday";
 })
 export class HolidayPage {
 
-    date: Date;
-    holidaySlider: CalendarHolidaySlider;
-    locale: String;
+    private date: Date;
+    private locale: String;
+    private holidays: HappyHoliday[] = [];
 
     constructor(public platform: Platform,
                 private navParams: NavParams,
                 public translateService: TranslateService) {
         this.date = navParams.get('date');
-        this.holidaySlider = new CalendarHolidaySlider(this.holidays());
+        this.holidays = this.getHolidays().filter(h => h);
+        console.log(JSON.stringify(this.holidays));
         this.locale = translateService.currentLang;
     }
 
-    holidays(): HappyHoliday[] {
+    getHolidays(): HappyHoliday[] {
         const worldHoliday = new WorldHoliday(this.date, this.translateService).get();
         const customHolidays = this.navParams.get('customHolidays');
         return customHolidays.concat(worldHoliday);
-    }
-
-    swipeHoliday($event): void {
-        if (Math.abs($event.deltaX) < 100) {
-            return;
-        }
-        if ($event.deltaX < 0) {
-            this.holidaySlider.next();
-
-        } else {
-            this.holidaySlider.previous();
-        }
     }
 
 }
